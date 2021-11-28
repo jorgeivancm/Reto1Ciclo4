0cadfacd41022c3401a9c2bc3cef24ed90e1c2e2
jorgeivancm <eijoto5516@gmail.com>
2021-11-27 19:02:22 -0500

    reto

---
+++ src/test/java/com/cuatroa/retouno/retouno/RetounoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuatroa.retouno.retouno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetounoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
